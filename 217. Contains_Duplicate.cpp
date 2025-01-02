71033c54178b1f3362aa2cab26afb063db3b5d48
AnamayNarkar <anamay.narkar.102@gmail.com>
2025-01-02 23:37:37 +0530

    217 && 242

---
+++ 217. Contains_Duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool containsDuplicate(vector<int> &nums) {
+
+        unordered_map<int, int> myMap;
+        for (int i : nums) {
+            if (myMap.find(i) == myMap.end()) {
+                myMap.insert(make_pair(i, 0));
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
---
+++ 242. Valid_Anagram.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> myMap;
+        for (char c : s) {
+            if (myMap.find(c) == myMap.end()) {
+                myMap.insert(make_pair(c, 0));
+            } else {
+                myMap[c]++;
+            }
+        }
+
+        unordered_map<char, int> myMap2;
+        for (char c : t) {
+            if (myMap2.find(c) == myMap2.end()) {
+                myMap2.insert(make_pair(c, 0));
+            } else {
+                myMap2[c]++;
+            }
+        }
+
+        if (myMap2.size() != myMap.size()) {
+            return false;
+        }
+
+        for (auto it = myMap.begin(); it != myMap.end(); it++) {
+            if (myMap2.find(it->first) == myMap2.end() || it->second != myMap2[it->first]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
