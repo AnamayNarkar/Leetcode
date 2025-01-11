161117602418830e5382be98c1e72ede31857eb2
AnamayNarkar <anamay.narkar.102@gmail.com>
2025-01-12 00:33:14 +0530

    704

---
+++ 704.Binary_Search.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int search(vector<int> &nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target < nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
