1c7d53a0575ed6750db4a558872f809c07108507
AnamayNarkar <anamay.narkar.102@gmail.com>
2025-02-06 00:32:02 +0530

    1790

---
+++ 1790.Check_if_One_String_Swap_Can_Make_Strings_Equal.go
@@ -0,0 +1,41 @@
+//go:build ignore
+
+package main 
+
+func areAlmostEqual(s1 string, s2 string) bool {
+
+    s1Len := len(s1)
+    s2Len := len(s2)
+
+    if s1Len!=s2Len {
+        return false
+    }
+
+    map1 := make(map[rune]int)
+    map2 := make(map[rune]int)
+
+    twoDone := 0
+
+    for index,_ := range s1 {
+        map1[rune(s1[index])]++
+        map2[rune(s2[index])]++
+
+        if rune(s1[index])!=rune(s2[index]) {
+            if twoDone!=2{
+                twoDone++
+            }else{
+                return false
+            }
+        }
+
+    }
+
+    for key,_ := range map1{
+        if map1[key] != map2[key]{
+            return false
+        }
+    }
+
+    return true
+
+}
