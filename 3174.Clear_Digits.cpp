25124672210ac6a9d1caab1fd8d798e383026e74
AnamayNarkar <anamay.narkar.102@gmail.com>
2025-02-12 14:06:03 +0530

    3174

---
+++ 3174.Clear_Digits.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    string clearDigits(string s) {
+        string ans = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] >= '0' && s[i] <= '9')
+                ans.pop_back();
+            else
+                ans += s[i];
+        }
+        return ans;
+    }
+};
