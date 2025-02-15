38c9fe3187639b9f15dfda7a818a19ab6c2f3e72
AnamayNarkar <anamay.narkar.102@gmail.com>
2025-02-15 15:33:57 +0530

    2698

---
+++ 2698.Find_the_Punishment_Number_of_an_Integer.cpp
@@ -0,0 +1,33 @@
+class Solution {
+
+    vector<pair<int, int>> prefix;
+
+    bool check(string &square, int num, int index, int strLen, int currSum) {
+        if (index == strLen) {
+            return currSum == num;
+        }
+
+        for (int i = index; i < strLen; ++i) {
+            int sum = stoi(square.substr(index, i - index + 1));
+            if (check(square, num, i + 1, strLen, currSum + sum))
+                return true;
+        }
+        return false;
+    }
+
+  public:
+    Solution() : prefix(1001) {
+        int sum = 0;
+        for (int i = 1; i <= 1000; ++i) {
+            prefix[i].first = i * i;
+            string temp = to_string(prefix[i].first);
+            if (check(temp, i, 0, temp.length(), 0)) {
+                sum += prefix[i].first;
+                // cout<<i<<" is good sum is"<<sum<<endl;
+            }
+            prefix[i].second = sum;
+        }
+    }
+
+    int punishmentNumber(int n) { return prefix[n].second; }
+};
